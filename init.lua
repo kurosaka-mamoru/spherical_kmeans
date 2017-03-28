44b637b99a0da1ad412fe669c6a6f386f21d77c5
kurosaka mamoru <k1630040@edu.cc.uec.ac.jp>
2017-03-28 21:10:48 +0900

    change max_iter

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ function spkmeans.use_th(x, k, th, batch_size, std)
     batch_size = batch_size or 10000
     std = std or 0.1
     
-    max_iter = 500
+    max_iter = 2000
     
     -- resize data and dims
     local nsamples = x:size(1)
